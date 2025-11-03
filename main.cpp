504f9bf8ce3f8726718518b00d10de826419a6d2
dumitruandrei13 <andreidragos1309@gmail.com>
2025-11-03 15:14:48 +0200

    Finalizează proiectul de simulare bancară și corectează logica de tranzacție
    Implementează clasele Manager, Banca (folosind compoziție cu Manager) și Client.
    Introduce o funcție privată (Verificare) pentru a asigura integritatea tranzacțiilor și blochează operațiunile cu sume negative sau zero.
    
     Logica funcțiilor transfer, depozit și credit a fost corectată pentru a asigura un flux monetar corect între Client și Bancă și pentru a verifica fondurile disponibile.
    
    Implementează constructorul fără parametri (cu mesaj de avertizare), constructorul de copiere și operatorul de atribuire (=) pentru clasa Client.
    Supraîncărcarea operatorului << pentru toate clasele (Manager, Banca, Client).

--- main.cpp
+++ main.cpp
@@ -1,50 +1,218 @@
 #include <iostream>
-#include <array>
+#include <string>
+using namespace std;
+
+
+class Banca;
+
+class Manager {
+private:
+    string nume;
+    string departament;
+    double salariu;
+
+public:
+    Manager(const string nume, const string departament, const double salariu) {
+        this->nume = nume;
+        this->departament = departament;
+        this->salariu = salariu;
+    }
+
+    void prezintaBanca(const Banca& b) const;
+
+    friend ostream& operator<<(ostream& os, const Manager& m);
+};
+
+ostream& operator<<(ostream& os, const Manager& m) {
+    os << "Manager: " << m.nume << "\n";
+    os << "Departament: " << m.departament << "\n";
+    os << "Salariu: " << m.salariu << " RON\n";
+    return os;
+}
+
+
+class Banca {
+private:
+    string nume;
+    string locatie;
+    int sumabanca;
+
+
+
+    Manager manager;
+
+public:
+
+    Banca(const string nume, const string locatie,
+          const string numeManager, const string depManager, double salariuManager)
+        : nume(nume),
+          locatie(locatie),
+          sumabanca(0),
+          manager(numeManager, depManager, salariuManager)
+    {
+
+    }
+
+
+    string getNume() const {
+        return nume;
+    }
+
+    void SetAdaugaSuma(int valoare) {
+        this->sumabanca += valoare;
+    }
+    int getSumaBanca() const {
+        return sumabanca;
+    }
+
+
+    const Manager& getManager() const {
+        return manager;
+    }
+
+
+    void prezintaManagerulBanca() const {
+        manager.prezintaBanca(*this);
+    }
+
+    friend ostream& operator<<(ostream& os, const Banca& b);
+};
+
+
+
+
+void Manager::prezintaBanca(const Banca& b) const {
+
+    cout << this->nume << " (" << this->departament << ") va prezinta banca " << b.getNume() << ".\n";
+}
+
+
+ostream& operator<<(ostream& os, const Banca& b) {
+    os << "Banca: " << b.nume << "\n";
+    os << "Locatia: " << b.locatie << "\n";
+    os << "Suma in banca: " << b.sumabanca << "\n";
+    os << "--- Managerul Bancii ---\n" << b.getManager();
+    return os;
+}
+
+
+class Client {
+private:
+    string nume;
+    int suma;
+    bool Verificare(int valoare) const {
+        if (valoare>=0)
+            return 1;
+        else
+            return 0;
+    }
+public:
+    Client(): nume("Nume lipsa"), suma(0)
+    {cout<< "Datele acestui client nu au fost introduse!\n";
+    }
+    Client(const string nume, const int suma) {
+        this->nume = nume;
+        this->suma = suma;
+    }
+Client& operator=(const Client& other) {
+        this->nume = other.nume;
+        this->suma = other.suma;
+        return *this;
+    }
+    void transfer(Client& dest, int valoare) {
+        if (!Verificare(valoare)) return;
+
+        if (suma >= valoare) {
+            suma -= valoare;
+            dest.suma += valoare;
+        } else {
+            cout << "Fonduri insuficiente pentru transfer!\n";
+        }
+    }
+
+
+    void depozit(Banca& b, int valoare) {
+        if (!Verificare(valoare)) return;
+
+        if (suma >= valoare) {
+            suma -= valoare;
+            b.SetAdaugaSuma(valoare);
+        } else {
+            cout << "Fonduri insuficiente pentru depozit!\n";
+        }
+    }
+
+    void credit(Banca& b, int valoare) {
+
+        if (!Verificare(valoare)) return;
+
+
+        if (b.getSumaBanca() >= valoare) {
+
+            suma += valoare;
+            b.SetAdaugaSuma(-valoare);
+        }
+        else
+        {
+            cout << "Banca nu are fondurile suficiente pentru un credit!\n";
+        }
+    }
+
+
+~Client() {
+        cout<<"\n Clientul a fost sters: "<<this->nume<<"\n";
+    }
+
+    friend ostream& operator<<(ostream& os, const Client& c);
+};
+
+ostream& operator<<(ostream& os, const Client& c) {
+    os << "Nume: " << c.nume << "\n";
+    os << "Suma: " << c.suma << "\n";
+    return os;
+}
+
 
 int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
+    Client c1("Andrei", 300);
+    Client c2("Maria", 300);
+    Client c4=c1;
+    Client c5;
+
+    Banca b("SmartBank", "Bucuresti", "Popescu Ion", "Sucursala", 5500.00);
+
+    Client c3("Alex",200);
+
+
+    // --- Transfer ---
+    c1.transfer(c2, 100);
+    cout << "\n--- Dupa Transfer ---\n";
+    cout << "C1: \n" << c1;
+    cout << "C2: \n" << c2;
+
+    // --- Depozit ---
+    c2.depozit(b, 50);
+    cout << "\n--- Dupa Depozit ---\n";
+    cout << "C2: \n" << c2;
+    cout << "Banca: \n" << b;
+
+    //--- Credit ---
+    c3.credit(b, 30);
+    cout << "\n--- Dupa Credit ---\n";
+    cout << "C3: \n" << c3;
+    cout << "Banca: \n" << b;
+
+    // --- Copiere ---
+    cout<<"\n--- Comparam la final constructorul de copiere---\n";
+    cout << "C1: \n" << c1;
+    cout << "C4: \n" << c4;
+
+    // --- Constructor fara parametru ---
+    cout<<"\n--- Verificam constructorul fara parametru---\n";
+    cout << "C5: \n" << c5;
+
+    cout << "\n--- Prezentare Manager (prin Banca) ---\n";
+    b.prezintaManagerulBanca();
+
     return 0;
 }
